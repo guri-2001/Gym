03c54f20211eb23a217f1f1d4e72a1afb40ff2c9
Gurbir Singh <gurbirsingh529@gmail.com>
2024-10-14 01:56:57 +0530

    update mongodb url

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,12 +1,7 @@
 const mongoose = require("mongoose");
 
-// const DB = "mongodb+srv://codecrafters:Devteam2024@data.ma9zn4r.mongodb.net/companyRecords?retryWrites=true&w=majority&appName=data"
-// const DB = process.env.MONGODB_URL
 
-// console.log(DB);
-
-
-mongoose.connect('mongodb+srv://codecrafters:Devteam2024@data.ma9zn4r.mongodb.net/companyRecords?retryWrites=true&w=majority&appName=data',{
+mongoose.connect('mongodb+srv://Zenith:zenith1234@gym.gbjxh.mongodb.net/Zenith?retryWrites=true&w=majority&appName=Gym',{
     useUnifiedTopology: true,
     useNewUrlParser: true
 }).then(()=>console.log("DATABASE connected")).catch((err)=> console.log("error" + err.message))
